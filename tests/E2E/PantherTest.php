20e7ba25e6c823fee3a14be3b0d74f1116117b72
AurelOconnell <aurelienoconnell@gmail.com>
2024-07-24 16:15:43 +0200

    adding panther E2E test

---
+++ tests/E2E/PantherTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Component\Panther\PantherTestCase;
+
+class PantherTest extends PantherTestCase
+{
+    public function testLoginAndNavigateToProfile()
+    {
+        $client = static::createPantherClient();
+
+        $crawler = $client->request('GET', '/login');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+
+        $form = $crawler->selectButton('Se connecter')->form();
+        $form['_username'] = 'votre_nom_utilisateur';
+        $form['_password'] = 'votre_mot_de_passe';
+
+        $client->submit($form);
+
+        $crawler = $client->followRedirect();
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+
+        $crawler = $client->click($crawler->selectLink('Mon profil')->link());
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+
+        $this->assertSelectorTextContains('h1', 'Profil');
+    }
+}
